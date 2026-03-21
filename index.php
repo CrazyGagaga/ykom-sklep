b7d788ad43ce8306f17b5a0750b65571ca20e170
CrazyGagaga <71091320+CrazyGagaga@users.noreply.github.com>
2026-03-21 11:46:06 +0100

    Dodałem navBara w wersji na telefony, trza zrobić tera do wersji na desktop :(

--- index.php
+++ index.php
@@ -5,17 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Y-KOM</title>
     <link rel="stylesheet" href="style.css">
+    <script>
+        function toggleMenu() {
+            console.log(document.getElementById("menu").classList.value);
+            document.getElementById("menu").classList.toggle("active");
+        }
+    </script>
 </head>
 <body>
     <header>
-        <h1>Y-KOM - Twój sklep komputerowy!</h1>
+        <h1>Y-KOM</h1>
         <div class="searchBar">
+            <div class="navBarRollUp">
+                <button id="rollUp" onclick="toggleMenu()"><img src="img/menu.png"/></button>
+                <div id="menu" class="dropdown-content">
+                    <div class="menu-header">
+                        <h3>Kategorie</h3>
+                        <button id="rollDown" onclick="toggleMenu()"><img src="img/back.png"/></button>
+                    </div>
+                    <a href="#">Laptopy i Komputer</a>
+                    <a href="#">Smartfony i Smartwatche</a>
+                    <a href="#">Podzespoły Komputerowe</a>
+                </div>
+            </div>
             <form action="" id="searchForm">
-                <input type="text" name="searchText" id="searchText">
+                <input type="text" name="searchText" id="searchText" placeholder="Wyszukaj...">
                 <button type="submit" name="searchSubmit" id="searchSubmit"> 
                     <img src="img/search.png"/>
                 </button>
             </form>
+        </div>
+        <div class="accountDetails">
+
         </div>
     </header>
     <main>
@@ -32,5 +53,6 @@
     <footer>
         
     </footer>
+    
 </body>
 </html>
