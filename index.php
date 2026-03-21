0faf7659211bcc5ecd36f5d1b53cb51c33669f0b
CrazyGagaga <71091320+CrazyGagaga@users.noreply.github.com>
2026-03-21 13:04:34 +0100

    dodałem footera, proszę zrób go ładniej bo ja nwm

--- index.php
+++ index.php
@@ -41,17 +41,25 @@
     </header>
     <main>
 
-
-    <?php
-
-
-
-
-    ?>
-
     </main>
+    <hr>
     <footer>
-        
+        <div id="orderSec">
+            <h3>Zamówienia</h3>
+            <a href="">Dostawa i płatności</a>
+        </div>
+        <div id="salesSec">
+            <h3>Promocje</h3>
+            <a href="">Wyprzedaż</a>
+        </div>
+        <div id="aboutSec">
+            <h3>y-kom</h3>
+            <a href="">O nas!</a>
+        </div>
+        <div id="contactSec">
+            <h3>Kontakt</h3>
+            <img src="img/phone.png"/><p> +48 123 456 789</p>
+        </div>
     </footer>
     
 </body>
---
+++ ykom-sklep-1774091480.sql
@@ -0,0 +1,56 @@
+CREATE TABLE IF NOT EXISTS `produkt` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`nazwa_prod` decimal(50) NOT NULL,
+	`opis_prod` varchar(250) NOT NULL,
+	`id_kategoria` int NOT NULL,
+	`cena_sztuka` decimal(10,0) NOT NULL,
+	`ilosc_magazyn` int NOT NULL,
+	`zdjecie_prod` blob NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `kategorie` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`nazwa_kat` varchar(50) NOT NULL,
+	`opis_kat` int NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `dane_uzyt_zam` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`imie` varchar(50) NOT NULL,
+	`nazwisko` varchar(50) NOT NULL,
+	`nr_dom` varchar(4) NOT NULL,
+	`ulica` varchar(50) NOT NULL,
+	`miejscowosc` varchar(50) NOT NULL,
+	`kod_poczt` varchar(6) NOT NULL,
+	`nr_tel` varchar(12) NOT NULL,
+	`email` varchar(50) NOT NULL,
+	`haslo` varchar(50) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `zamowienia` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`id_osob` int NOT NULL,
+	`data_zamow` datetime NOT NULL,
+	`status` varchar(50) NOT NULL,
+	`kwota` decimal(10,0) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `zamow_produkty` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`id_zamow` int NOT NULL,
+	`id_produkt` int NOT NULL,
+	`ilosc` int NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `produkt` ADD CONSTRAINT `produkt_fk3` FOREIGN KEY (`id_kategoria`) REFERENCES `kategorie`(`id`);
+
+
+ALTER TABLE `zamowienia` ADD CONSTRAINT `zamowienia_fk1` FOREIGN KEY (`id_osob`) REFERENCES `dane_uzyt_zam`(`id`);
+ALTER TABLE `zamow_produkty` ADD CONSTRAINT `zamow_produkty_fk1` FOREIGN KEY (`id_zamow`) REFERENCES `zamowienia`(`id`);
+
+ALTER TABLE `zamow_produkty` ADD CONSTRAINT `zamow_produkty_fk2` FOREIGN KEY (`id_produkt`) REFERENCES `produkt`(`id`);
