ada1a266db767c88e73d1d9f61d90629e0db35b9
reald4gnk <dragonek.gg@gmail.com>
2026-03-21 13:20:46 +0100

    dodalem plik z karta produktu oraz polaczylem z css (poki co osobny plik)

---
+++ produkt.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Karta produktu</title>
+    <link rel="stylesheet" href="produktstyle.css">
+</head>
+<body>
+    <div class="karta_produktu">
+    <img src="img/no_name.png" alt="no_image">
+    <h3>Komputer YKOM i5-14600KF/RTX4060/32GB RAM/2TB SSD</h3>
+
+
+</div>
+    
+</body>
+</html>
+
+
