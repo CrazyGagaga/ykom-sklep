fc102c11883fced45ca8623b4429b6d1151540c9
reald4gnk <dragonek.gg@gmail.com>
2026-03-25 09:23:09 +0100

    wprowadzono wstepna rejestracje uzytkownika

--- rejestracja.php
+++ rejestracja.php
@@ -15,17 +15,35 @@
     <main>
         <div class="rejestracjamain">
             <h2>Zarejestruj się!</h2><br>
-        <form>
+        <form method="POST" action="rejestracja.php">
             <label>login: </label><br>
-            <input type="text"><br><br>
+            <input type="text" name="login"><br><br>
             <label>email: </label><br>
-            <input type="text"><br><br>
+            <input type="text" name="email"><br><br>
             <label>Hasło: </label><br>
-            <input type="password"><br><br>
+            <input type="password" name="password"><br><br>
             <label>Powtórz hasło: </label><br>
-            <input type="password"><br><br>
+            <input type="password" name="repeatpassword"><br><br>
             <button onclick="">Zarejestruj się</button>
         </form><br>
+        <?php
+        $conn = mysqli_connect("localhost", "root", "", "ykom_baza");
+        
+        $login = mysqli_real_escape_string($conn, $_POST["login"]);
+        $email = mysqli_real_escape_string($conn, $_POST["email"]);
+        $pass = mysqli_real_escape_string($conn, $_POST["password"]);
+$repeatpassword = mysqli_real_escape_string($conn, $_POST["repeatpassword"]);
+        if ($login == "" || $email == "" || $pass == "" || $repeatpassword == "") {
+            echo "Nie wprowadzono wszystkich danych!";
+        }
+        else if ($repeatpassword != $pass) {
+            echo "Powtórzone hasło jest nieprawidłowe!";
+        }
+        else {
+        $q1 = "INSERT INTO `dane_uzyt_zam` (`id`, `imie`, `nazwisko`, `nr_dom`, `ulica`, `miejscowosc`, `kod_poczt`, `nr_tel`, `email`, `login`, `haslo`) VALUES (NULL, '', '', '', '', '', '', '', '$email', '$login', '$pass');";
+        $result1 = mysqli_query($conn, $q1);
+        }
+?>
         <p>Posiadasz już konto?
         </p>
         <a href="loguj.php">Zaloguj się</a>
