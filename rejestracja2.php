171944c09bf9112b56b006e66533e1650b965a8f
reald4gnk <dragonek.gg@gmail.com>
2026-03-25 21:26:19 +0100

    ogarniecie wygladu rejestracja2.php oraz pobierania zawartosci z formularza

--- rejestracja2.php
+++ rejestracja2.php
@@ -13,24 +13,50 @@
     <main>
         <div class="rejestracjamain">
             <h2>Zarejestruj się!</h2><br>
-        <form>
+        <form method="POST" action="rejestracja2.php">
             <label>Imie: </label><br>
-            <input type="text"><br><br>
+            <input type="text" name="imie"><br><br>
             <label>Nazwisko: </label><br>
-            <input type="text"><br><br>
+            <input type="text" name="nazwisko"><br><br>
             <label>Ulica: </label><br>
-            <input type="text"><br><br>
+            <input type="text" name="ulica"><br><br>
             <label>Numer domu: </label><br>
-            <input type="text"><br><br>
+            <input type="text" name="numer_domu"><br><br>
             <label>Miejscowość: </label><br>
-            <input type="text"><br><br>
+            <input type="text" name="miejscowosc"><br><br>
             <label>Kod pocztowy: </label><br>
-            <input type="text"><br><br>
+            <input type="text" name="kod_pocztowy"><br><br>
             <label>Numer telefonu: </label><br>
-            <input type="text"><br><br>
+            <input type="text" name="nr_tel"><br><br>
             <p>Dane te są wymagane do poprawnego składania zamówienia</p><br>
-            <button onclick="">Ukończ rejestrację</button>
+            <button id="rejestracjabutton">Ukończ rejestrację</button>
         </form><br>
+
+        <?php 
+        $conn = mysqli_connect("localhost", "root", "", "ykom_baza");
+        
+        $imie = $_POST['imie'];
+        $nazwisko = $_POST['nazwisko'];
+        $ulica = $_POST['ulica'];
+        $numer_domu = $_POST['numer_domu'];
+        $miejscowosc = $_POST['miejscowosc'];
+        $kod_pocztowy = $_POST['kod_pocztowy'];
+        $nr_tel = $_POST['nr_tel'];
+
+
+        echo $imie . $nazwisko . $ulica . $numer_domu . $miejscowosc . $kod_pocztowy . $nr_tel;
+        
+        
+        
+        
+        
+        
+        
+        ?>
+
+
+
+
         </div>
     </main>
 </body>
