c24a2f4864f859a61a23c2eb6154bc4c1e0a8278
reald4gnk <dragonek.gg@gmail.com>
2026-04-12 17:39:22 +0200

    male poprawki stylistyczne

--- loguj.php
+++ loguj.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>YKOM - Zaloguj się!</title>
+    <title>Zaloguj się - YKOM</title>
     <link rel="stylesheet" href="css\logujstyle.css">
 </head>
 <body>
--- rejestracja.php
+++ rejestracja.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>YKOM - Zaloguj się!</title>
+    <title>Zarejestruj się - YKOM</title>
     <link rel="stylesheet" href="css\rejestracjastyle.css">
 </head>
 <body>
